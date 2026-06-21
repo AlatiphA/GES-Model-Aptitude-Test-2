6e65d0be63339d75c6b7eacc3b1331b3ffc9382e
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-06-21 21:25:08 +0000

    Update app version to v3.0.5

--- sw-mat2.js
+++ sw-mat2.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION  = "v3.0.1";
+const APP_VERSION  = "v3.0.5";
 const CACHE_NAME   = `model-aptitude-test2-${APP_VERSION}`;
 
 const FILES_TO_CACHE = [
