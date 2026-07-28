53fd95c75f180bcb812808efea186b9f7ba2706b
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-07-28 09:54:03 +0000

    Update data keys for reader and bookmarks

--- app.js
+++ app.js
@@ -170,10 +170,10 @@ if (versionEl)
     "v" + APP_VERSION;
 
 const READER_DATA_KEY =
-  "ges-pasco-data";
+  "ges-mat2-data";
 
 const BOOKMARKS_KEY =
-  "ges-pasco-bookmarks";
+  "ges-mat2-bookmarks";
 
 
 /* =========================
